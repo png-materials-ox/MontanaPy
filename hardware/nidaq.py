f16a720636940341fe223d896f09d07df7b47033
JAMS <JAMS@oums-jams12.materials.ox.ac.uk>
2023-04-08 10:03:11 +0100

    Reverted back - cant do this

--- hardware/nidaq.py
+++ hardware/nidaq.py
@@ -15,15 +15,6 @@ class DAQ:
 
         # self.fsm_x_chan = self.daq + "/ai0"              # Fast steering mirror x
         # self.fsm_y_chan = self.daq + "/ai1"              # Fast steering mirror y
-        try:
-            self.task = nidaqmx.Task()
-        except nidaqmx.DaqError as e:\
-            print("An error occurred:", e)
-        except Exception as e:
-            print("An unexpected error occurred:", e)
-        finally:
-            self.task.close()
-
 
     def counter(self, sample_time):
         '''
@@ -31,34 +22,46 @@ class DAQ:
         :param sample_time: sampling time for the connected single photon counter (milliseconds)
         :return: number of photon counts in one second
         '''
-        self.task.ci_channels.add_ci_count_edges_chan(self.ctr_chan)
-        self.task.ci_channels[0].ci_count_edges_term = self.photon_term
+        try:
+            with nidaqmx.Task() as task:
+                task.ci_channels.add_ci_count_edges_chan(self.ctr_chan)
+                task.ci_channels[0].ci_count_edges_term = self.photon_term
 
-        self.task.start()
-        time.sleep(sample_time)
-        cnt0 = self.task.read()
-        time.sleep(sample_time)
+                task.start()
+                time.sleep(sample_time)
+                cnt0 = task.read()
+                time.sleep(sample_time)
 
-        cnt1 = self.task.read()
-        self.task.stop()
-        phot_cnt = (cnt1 - cnt0) * (sample_time) ** -1
-        return phot_cnt
+                cnt1 = task.read()
+                task.stop()
+                phot_cnt = (cnt1 - cnt0) * (sample_time) ** -1
+                return phot_cnt
+        except nidaqmx.DaqError as e:\
+            print("An error occurred:", e)
+
+        except Exception as e:
+            print("An unexpected error occurred:", e)
+
+        finally:
+            task.close()
 
     def scan_voltage(self):
         pass
 
-    def add_ai_channel(self, channel):
-        self.task.ai_channels.add_ai_voltage_chan(channel)
-
     def read_analogue_voltage(self, channel="ai0", min=-10, max=10, rate=1000, n_samps=1):
-        self.task.ai_channels.all.ai_min = min
-        self.task.ai_channels.all.ai_max = max
+        # Create a DAQ session
+        with nidaqmx.Task() as task:
+            # Configure the input channels
+            task.ai_channels.add_ai_voltage_chan(channel)
 
-        # Set the acquisition parameters
-        self.task.timing.cfg_samp_clk_timing(rate=rate, sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
+            task.ai_channels.all.ai_min = min
+            task.ai_channels.all.ai_max = max
 
-        # Start the acquisition
-        self.task.start()
-        data = self.task.read(number_of_samples_per_channel=n_samps)
-        self.task.stop()
-        return data
+            # Set the acquisition parameters
+            task.timing.cfg_samp_clk_timing(rate=rate, sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
+
+            # Start the acquisition
+            task.start()
+            data = task.read(number_of_samples_per_channel=n_samps)
+            task.stop()
+            return data
